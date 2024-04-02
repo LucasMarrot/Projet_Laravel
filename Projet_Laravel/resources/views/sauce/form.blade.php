e4b500d0a103c81d63b4dcc74b2876b24a1324a2
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2024-04-02 15:29:21 +0200

    Design form p1

--- Projet_Laravel/resources/views/sauce/form.blade.php
+++ Projet_Laravel/resources/views/sauce/form.blade.php
@@ -1,55 +1,55 @@
-<form method="POST" action="" enctype="multipart/form-data">
+<form method="POST" action="" enctype="multipart/form-data" class="sauce-form">
     @csrf
-    <div>
+    <div class="form-group">
         <label for="userId">UserId :</label>
         <input type="text" id="userId" name="userId" value="{{ old('userId', $sauce->userId) }}">
         @error('userId')
-            {{ $message }}
+            <span class="error-message">{{ $message }}</span>
         @enderror
     </div>
-    <div>
+    <div class="form-group">
         <label for="name">Nom de la sauce :</label>
         <input type="text" id="name" name="name" value="{{ old('name', $sauce->name) }}">
         @error('name')
-            {{ $message }}
+            <span class="error-message">{{ $message }}</span>
         @enderror
     </div>
-    <div>
+    <div class="form-group">
         <label for="manufacturer">Fabricant :</label>
         <input type="text" id="manufacturer" name="manufacturer"  value="{{ old('manufacturer', $sauce->manufacturer) }}">
         @error('manufacturer')
-            {{ $message }}
+            <span class="error-message">{{ $message }}</span>
         @enderror
     </div>
-    <div>
+    <div class="form-group">
         <label for="description">Description :</label>
         <textarea id="description" name="description" >{{ old('description', $sauce->description) }}</textarea>
         @error('description')
-            {{ $message }}
+            <span class="error-message">{{ $message }}</span>
         @enderror
     </div>
-    <div>
+    <div class="form-group">
         <label for="mainPepper">Ingrédient principal :</label>
         <input type="text" id="mainPepper" name="mainPepper"  value="{{ old('mainPepper', $sauce->mainPepper) }}">
         @error('mainPepper')
-            {{ $message }}
+            <span class="error-message">{{ $message }}</span>
         @enderror
     </div>
-    <div>
+    <div class="form-group">
         <label for="imageUrl">URL de l'image de la sauce :</label>
         <input type="text" id="imageUrl" name="imageUrl"  value="{{ old('imageUrl', $sauce->imageUrl) }}">
         @error('imageUrl')
-            {{ $message }}
+            <span class="error-message">{{ $message }}</span>
         @enderror
     </div>
-    <div>
+    <div class="form-group">
         <label for="heat">Heat (1-10) :</label>
         <input type="number" id="heat" name="heat" min="1" max="10"  value="{{ old('heat', $sauce->heat) }}">
         @error('heat')
-            {{ $message }}
+            <span class="error-message">{{ $message }}</span>
         @enderror
     </div>
-    <button type="submit">
+    <button type="submit" class="view-more">
         @if ($sauce -> id)
             Modifier la sauce
         @else
