d0625cea1b2240b34ca4cf5b96c0c821170ff6d0
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2024-04-02 08:56:25 +0200

    Front

--- Projet_Laravel/resources/views/base.blade.php
+++ Projet_Laravel/resources/views/base.blade.php
@@ -5,18 +5,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/base.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
 
-    <div class="container">
-        @if (session('success'))
-            <div>
-                {{session('sucess')}}
-            </div>  
-        @endif
-        
-        @yield('content')
+<header>
+    <nav>
+        <ul>
+            <li><a href="/" class="@yield('home_active')">Accueil</a></li>
+            <li><a href="/sauce" class="@yield('catalogue_active')">Catalogue</a></li>
+            <li><a href="/sauce/new" class="@yield('add_sauce_active')">Ajouter une sauce</a></li>
+        </ul>
+    </nav>
+    <a href="/" class="logo">
+        <img src="{{ asset('img/HotTake_Logo.png') }}" alt="Hot takes logo">
+    </a>
+    <div class="auth">
+        <a href="#">Se connecter</a>
+        <a href="#">S'enregistrer</a>
     </div>
+</header>
+
+<div class="container">
+    @if (session('success'))
+        <div class="success-message">
+            {{ session('success') }}
+        </div>
+    @endif
     
+    @yield('content')
+</div>
+
 </body>
 </html>
--- Projet_Laravel/resources/views/sauce/index.blade.php
+++ Projet_Laravel/resources/views/sauce/index.blade.php
@@ -4,20 +4,28 @@
 
 @section('content')
 
-    <h1>Les sauces</h1>
-
     @foreach ($sauces as $sauce)
-        <sauce>
+        <div class="sauce">
             <h2>{{ $sauce->name }}</h2>
             <p><strong>Fabricant:</strong> {{ $sauce->manufacturer }}</p>
             <p><strong>Description:</strong> {{ $sauce->description }}</p>
             <p><strong>Ingrédient principal:</strong> {{ $sauce->mainPepper }}</p>
             <img src="{{ $sauce->imageUrl }}" alt="Image de la sauce {{ $sauce->name }}">
-            <p><strong>Heat:</strong> {{ $sauce->heat }}/10</p>
-            <p><strong>Likes:</strong> {{ $sauce->likes }}</p>
-            <p><strong>Dislikes:</strong> {{ $sauce->dislikes }}</p>
-            <a href="{{ route('sauce.show', ['sauce' => $sauce->id]) }}">Voir la sauce</a>
-        </sauce>
+            <div class="heat" style="font-size: 24px; text-align: center;">
+                <span style="color: {{ $sauce->heat <= 3 ? '#f5d547' : ($sauce->heat <= 6 ? '#ffa500' : '#ff6347') }}">{{ $sauce->heat }}/10</span>
+                <i class="fas fa-fire" style="color: {{ $sauce->heat <= 3 ? '#f5d547' : ($sauce->heat <= 6 ? '#ffa500' : '#ff6347') }}"></i>
+            </div>
+            <div class="reactions">
+                <div class="like">
+                    <i class="fas fa-thumbs-up"></i> {{ $sauce->likes }}
+                </div>
+                <div class="dislike">
+                    <i class="fas fa-thumbs-down"></i> {{ $sauce->dislikes }}
+                </div>
+            </div>
+            
+            <a class="view-more" href="{{ route('sauce.show', ['sauce' => $sauce->id]) }}">Voir la sauce</a>
+        </div>
     @endforeach
 
     {{ $sauces -> links() }}
