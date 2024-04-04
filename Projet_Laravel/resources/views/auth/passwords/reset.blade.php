94e9590ff36bf2c9dd9979903452048b98eadbde
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2024-04-04 21:29:38 +0200

    CSS Reset password

--- Projet_Laravel/resources/views/auth/passwords/reset.blade.php
+++ Projet_Laravel/resources/views/auth/passwords/reset.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('base')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <h1 class="form-title">{{ __('Reset Password') }}</h1>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
@@ -13,7 +13,7 @@
 
                         <input type="hidden" name="token" value="{{ $token }}">
 
-                        <div class="row mb-3">
+                        <div class="form-group row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
@@ -27,7 +27,7 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
+                        <div class="form-group row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
@@ -41,7 +41,7 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
+                        <div class="form-group row mb-3">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
@@ -49,13 +49,13 @@
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                        
+                            <div class="form-button">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Reset Password') }}
                                 </button>
                             </div>
-                        </div>
+                        
                     </form>
                 </div>
             </div>
